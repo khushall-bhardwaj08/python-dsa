b53a5edab07f0847083b6be18ad562aee981f6d3
khushall-bhardwaj08 <bkhushal97@gmail.com>
2026-08-15 11:41:03 +0530

    Time: 5 ms (25.78%) | Memory: 13.3 MB (85.65%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        left = 0
+        right = 1
+
+        while right < len(nums):
+            if nums[left] == nums[right]:
+                right += 1
+            else:
+                left += 1
+                nums[left] = nums[right]
+                right += 1
+
+        return left + 1
